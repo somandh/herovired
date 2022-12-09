f391c73a809a9d09de8b516f865f54d0c53b37ad
Somanadh8249 <somanadh8249@gmail.com>
2022-12-09 10:41:49 +0530

    first commit of list,tuple and dict

---
+++ hvlitudi.py
@@ -0,0 +1,52 @@
+# If Else
+#n = 10
+#if n<=0:
+# if (n<=0):
+#     print('less than zero')
+# elif (n==10):
+#     print('value is lessthan equal to five')
+# else:
+#     print('n is higher than ten')
+# students = ['rahul','ram','raj','ravi','raghu','rani','reshma','sagar']
+# print('The student names are :')
+# for s in students:
+#     print(s)
+l = [1 , 2 , 3.5 , 4.5 , 'aditya' , True ]
+# print(l)
+# print(l[0])
+# print(l[4])
+#print(l[1:3]) #include 1 and exclude 3
+#print(l[1:5])
+#print(l[2:])
+#print(l*2)
+l1=['sandeep','mohan']
+#print(l+l1)
+#print(l1+l)
+t = (1 , 2 , 3.5 , 4.5 , 'aditya' , True)
+# print(t[0])
+# print(t[1:4])
+# print(t[2:])
+details = { 'ram' : 90, 'rahul' : 80, 'satya' : 50, 'anik' : 100 }
+# details[4]= "Test"
+#print(details)
+# print(details.values())
+# print(details.keys())
+# print(details[4])
+# print(details)
+# dict = {
+#     "college" : "aditya",
+#     "location" : "andhra pradesh",
+#     "stream" : "ece"
+# }
+# for info in dict:
+#     print(info)
+# for info1 in dict:
+#     print(dict[info1])
+#college={}
+name = input("enter college name :")
+rollnum = input("enter Roll number :")
+#college[name]=rollnum
+#print(college)
+college1 = {name:rollnum}
+print(college1)
+Footer
